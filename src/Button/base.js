437a0cd1c24bacc91bd2aaf601ad342664c8aa1e
Jay Chung <xjkit54780@gmail.com>
2017-08-06 22:12:10 +0800

    Add styled-component linter

--- src/Button/base.js
+++ src/Button/base.js
@@ -6,20 +6,24 @@ const base = css`
   min-width: 80px;
   margin: 0 4px;
   padding: 0 22px;
+
   &:focus {
     outline: 0;
-  };
+  }
+
   &:hover {
     opacity: 0.8;
-  };
+  }
+
   &:disabled {
     opacity: 0.5;
     cursor: not-allowed;
+
     &:active {
       box-shadow: inherit;
       color: inherit;
-    };
-  };
+    }
+  }
 `;
 
 export const defaultStyle = css`
@@ -27,10 +31,11 @@ export const defaultStyle = css`
   border: 1px solid ${colors.defaultBlack};
   background-color: #fff;
   color: ${colors.defaultBlack};
+
   &:active {
-    box-shadow: 0 0 0px 1px ${colors.defaultBlack} inset;
+    box-shadow: 0 0 0 1px ${colors.defaultBlack} inset;
     color: ${colors.defaultBlack};
-  };
+  }
 `;
 
 export const primaryStyle = css`
@@ -38,9 +43,10 @@ export const primaryStyle = css`
   border: 1px solid ${colors.primaryBlue};
   background-color: ${colors.primaryBlue};
   color: #fff;
+
   &:active {
     opacity: 1;
-  };
+  }
 `;
 
 export const successStyle = css`
@@ -48,9 +54,10 @@ export const successStyle = css`
   border: 1px solid ${colors.successGreen};
   background-color: ${colors.successGreen};
   color: #fff;
+
   &:active {
     opacity: 1;
-  };
+  }
 `;
 
 export const warningStyle = css`
@@ -58,9 +65,10 @@ export const warningStyle = css`
   border: 1px solid ${colors.warningOrange};
   background-color: ${colors.warningOrange};
   color: #fff;
+
   &:active {
     opacity: 1;
-  };
+  }
 `;
 
 export const dangerStyle = css`
@@ -68,9 +76,10 @@ export const dangerStyle = css`
   border: 1px solid ${colors.dangerRed};
   background-color: ${colors.dangerRed};
   color: #fff;
+
   &:active {
     opacity: 1;
-  };
+  }
 `;
 
 export const roundedButtonStyle = css`
