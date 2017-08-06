2263730a355fbdd818f29f033606a222b7238e80
Jay Chung <xjkit54780@gmail.com>
2017-08-06 23:20:39 +0800

    eject from creact-react-app

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,19 @@
+const path = require('path');
+
 module.exports = {
+  webpackConfig: {
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+      ]
+    }
+  },
   components: 'src/**/[A-Z]*.js'
 };
